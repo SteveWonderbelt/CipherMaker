d16020c64715495c7ed4b8942ba5061d6e403ad3
SteveWonderbelt <schilds@email.sc.edu>
2020-01-26 13:14:48 -0500

    flexbox stuff

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PresentationalComponent from './PresentationalComponent'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default class App extends React.Component {
-    state = {
-        myState: 'This is my state'
-    }
-
-    render() {
-        return (
-            <View>
-                <PresentationalComponent myState = {this.state.myState}/>
-            </View>
-        );
-    }
+const Home = (props) => {
+    return(
+        <View style = {styles.container}>
+            <View style= {styles.redbox} />
+            <View style = {styles.bluebox} />
+            <View style={styles.blackbox} />
+        </View>
+    )
 }
+
+export default Home
+
+const styles = StyleSheet.create ({
+    container:{
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+        alignItems : 'flex-end',
+        backgroundColor : 'grey',
+        height : 600
+    },
+    redbox:{
+        width:100,
+        height:100,
+        backgroundColor: 'red'
+    },
+    bluebox: {
+        width:100,
+        height: 100,
+        backgroundColor: 'blue'
+    },
+    blackbox:{
+        width:100,
+        height: 100,
+        backgroundColor:'black'
+    },
+})
+
+
