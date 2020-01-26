141e7885edb183ba9cf3f5639b23a9f50c130c97
SteveWonderbelt <schilds@email.sc.edu>
2020-01-26 13:33:02 -0500

    Listview stuff

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import List from './List.js'
 
-const Home = (props) => {
-    return(
-        <View style = {styles.container}>
-            <View style= {styles.redbox} />
-            <View style = {styles.bluebox} />
-            <View style={styles.blackbox} />
-        </View>
+const App = () => {
+    return (
+        <List />
     )
 }
 
-export default Home
-
-const styles = StyleSheet.create ({
-    container:{
-        flexDirection : 'row',
-        justifyContent : 'space-between',
-        alignItems : 'flex-end',
-        backgroundColor : 'grey',
-        height : 600
-    },
-    redbox:{
-        width:100,
-        height:100,
-        backgroundColor: 'red'
-    },
-    bluebox: {
-        width:100,
-        height: 100,
-        backgroundColor: 'blue'
-    },
-    blackbox:{
-        width:100,
-        height: 100,
-        backgroundColor:'black'
-    },
-})
-
-
+export default App
